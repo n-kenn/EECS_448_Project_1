85f45e1d93ec49d412fa2ecc7a6026d8339bfcf5
Unknown <nkenn@ku.edu>
2017-09-13 10:17:44 -0500

    Deleted a lot of methods to prepare to make them easier to read

--- mainwindow.h
+++ mainwindow.h
@@ -16,36 +16,7 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_btnNew_clicked();
-
-    void on_pushButton_2_clicked();
-
-    void on_pushButton_3_clicked();
-
-    void on_pushButton_5_clicked();
-
-    void on_pushButton_6_clicked();
-
-    void on_pushButton_7_clicked();
-
-    void on_btnSelecExist_clicked();
-
-    void on_pushButton_10_clicked();
-
-    void on_pushButton_9_clicked();
-
-    void on_pushButton_11_clicked();
-
-    void on_pushButton_12_clicked();
-
-    void on_pushButton_14_clicked();
-
-    void on_pushButton_15_clicked();
-
-    void on_pushButton_16_clicked();
-
-    void on_pushButton_clicked();
-
+    
 private:
     Ui::MainWindow *ui;
 };
