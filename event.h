6f9d91260c01ae22eefd0525a697715c73d254b4
thobrooks <t090b057@ku.edu>
2017-09-15 17:06:21 -0500

    Documentation Added. The Format Still Needs to be Tested.

--- event.h
+++ event.h
@@ -9,25 +9,128 @@ using namespace std;
 
 class Event {
 public:
+    /*!
+   * \brief Event - Constructor
+   *
+   * Pre: None
+   * Post: An empty event is created.
+   */
   Event();
+  /*!
+   * \brief Event - Constructor
+   * \param name
+   * \param date
+   * \param creator
+   *
+   * Pre: None
+   * Post: An event is created with multiple parts initialized.
+   */
   Event(const QString& name, const QString& date, const QString& creator);
+
+  /*!
+  * \brief Event - Deconstructor
+  *
+  * Pre: An event is to be deleted.
+  * Post: None
+  */
   ~Event();
+
+  /*!
+   * \brief setName
+   * \param name
+   *
+   * Pre: A name is to be added.
+   * Post: a name is added to the event.
+   */
   void setName(const QString& name);
+
+  /*!
+   * \brief getName
+   * \return name
+   *
+   * Pre: None
+   * Post: None
+   */
   QString getName() const;
+
+  /*!
+   * \brief getDate
+   * \return date
+   *
+   * Pre: None
+   * Post: None
+   */
   QString getDate() const;
+
+  /*!
+   * \brief getCreator
+   * \return creator
+   *
+   * Pre: None
+   * Post: None
+   */
   QString getCreator() const;
+
+  /*!
+   * \brief getSlots
+   * \return QVector<QString> timeSlots
+   *
+   * Pre: None
+   * Post: None
+   */
   QVector<QString> getSlots() const;
+
+  /*!
+   * \brief getAttendees
+   * \return attendees
+   *
+   * Pre: None
+   * Post: None
+   */
   QVector<QString> getAttendees() const;
+
+  /*!
+   * \brief addSlot
+   * \param slot
+   *
+   * Pre: a time slot is to be added to the event.
+   * Post: a time slot is added to the event.
+   */
   void addSlot(const QString& slot);
+
+  /*!
+   * \brief removeSlot
+   * \param slot
+   *
+   * Pre: A time slot is to be removed from the event.
+   * Post: A time slot is removed from the event.
+   */
   void removeSlot(const QString& slot);
+
+  /*!
+   * \brief addAttendee
+   * \param attendee
+   *
+   * Pre: An attendee is to be added to the vector.
+   * Post: An attendee is added.
+   */
   void addAttendee(const QString& attendee);
+
+  /*!
+   * \brief removeAttendee
+   * \param attendee
+   *
+   * Pre: An attendee is to be removed from the event.
+   * Post: An attendee is removed.
+   */
   void removeAttendee(const QString& attendee);
 private:
-  QString name;
-  QString creator;
-  QString date;
-  QVector<QString> timeSlots;
-  QVector<QString> attendees;
+  QString name; /*!< The name of the event.*/
+  QString creator; /*!< The name of the creator. */
+  QString date; /*!< the date that the event is going to take place at.*/
+  QVector<QString> timeSlots; /*!< The times the event will be taking place at. Multiple times in stagnated bunches might be used,
+                                   so a vector is used.*/
+  QVector<QString> attendees; /*!< The list of the attendees to an event.*/
 };
 
 #endif
--- mainwindow.h
+++ mainwindow.h
@@ -16,52 +16,184 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = 0);
+    /*!
+      \brief MainWindow - Deconstructor
+      *
+      * Pre: The MainWindow is to be deleted.
+      * Post: None
+      */
     ~MainWindow();
 
 private slots:
-    
+    /*!
+     * \brief on_btnNew_clicked
+     *
+     * Pre: None
+     * Post: The button is clicked, indicating that a new event is wanting to be created.
+     */
     void on_btnNew_clicked();
 
+    /*!
+     * \brief on_btnSelecExist_clicked
+     *
+     * Pre: None
+     * Post: The button is clicked, indicating that something is desired about the attendance.
+     */
     void on_btnSelecExist_clicked();
 
+    /*!
+     * \brief on_btnNewDateBack_clicked
+     *
+     * Pre: None
+     * Post: The user will return to pageStart.
+     */
     void on_btnNewDateBack_clicked();
 
+    /*!
+     * \brief on_btnNewDateNext_clicked
+     *
+     * Pre: None
+     * Post: The user will go to select a time for the new event.
+     */
     void on_btnNewDateNext_clicked();
 
+
+    /*!
+     * \brief on_btnNewTimeBack_clicked
+     *
+     * Pre: None
+     * Post: The user will return back to pick a date.
+     */
     void on_btnNewTimeBack_clicked();
 
+
+    /*!
+     * \brief on_btnNewTimeSave_clicked
+     *
+     * Pre: None
+     * Post: The date and times will be saved, and a new event will be created.
+     */
     void on_btnNewTimeSave_clicked();
 
+
+    /*!
+     * \brief on_btnNewTimeToggle_clicked
+     *
+     * Pre: None
+     * Post: The time format on the checkboxes will be changed to either 24 or 12 hour format depending on the value of
+     *       currentToggleNew.
+     */
     void on_btnNewTimeToggle_clicked();
 
+
+    /*!
+     * \brief on_btnExit_clicked
+     *
+     * Pre: None
+     * Post: The program is to end.
+     */
     void on_btnExit_clicked();
 
+
+    /*!
+     * \brief on_btnReturnToStart_clicked
+     *
+     * Pre: None
+     * Post: The user will return back to the start.
+     */
     void on_btnReturnToStart_clicked();
 
+    /*!
+     * \brief on_btnListAttendanceBack_clicked
+     *
+     * Pre: None
+     * Post: The user will return back to the beginning.
+     */
     void on_btnListAttendanceBack_clicked();
 
+
+    /*!
+     * \brief on_btnListAttendanceNext_clicked
+     *
+     * Pre: None
+     * Post: The user will go either to add their attendance to the selected event,
+     *       or they will go to see a list of attendees to events.
+     */
     void on_btnListAttendanceNext_clicked();
 
+
+    /*!
+     * \brief on_btnAddAttendanceBack_clicked
+     *
+     * Pre: None
+     * Post: The user will return back to pageListAttendance
+     */
     void on_btnAddAttendanceBack_clicked();
 
+    /*!
+     * \brief on_btnAddAttendanceSave_clicked
+     *
+     * Pre: None
+     * Post: The user's attendance to the selected event will be saved.
+     */
     void on_btnAddAttendanceSave_clicked();
 
+    /*!
+     * \brief on_btnViewAttendanceBack_clicked
+     *
+     * Pre: None
+     * Post: The user will return back to pageListAttendance
+     */
     void on_btnViewAttendanceBack_clicked();
 
+    /*!
+     * \brief on_btnViewAttendanceReturn_clicked
+     *
+     * Pre: None
+     * Post: The user will return back to the beginning of the program.
+     */
     void on_btnViewAttendanceReturn_clicked();
 
+    /*!
+     * \brief on_btnViewAttendanceQuit_clicked
+     *
+     * Pre: None
+     * Post: The program is to end.
+     */
     void on_btnViewAttendanceQuit_clicked();
 
+
+    /*!
+     * \brief on_txtName_textChanged
+     * \param arg1 - An unnecessary argument, but kept just in case for potential needs for evaluation.
+     *
+     * Pre: None
+     * Post: So long as the line edit is not empty, the buttons will become enabled.
+     */
     void on_txtName_textChanged(const QString &arg1);
 
+
+    /*!
+     * \brief on_rdAdd_clicked
+     *
+     * Pre: None
+     * Post: btnListAttendanceNext will be enabled.
+     */
     void on_rdAdd_clicked();
 
+    /*!
+     * \brief on_rdView_clicked
+     *
+     * Pre: None
+     * Post: btnLIstAttendanceNext will be enabled.
+     */
     void on_rdView_clicked();
 
 private:
-    QVector<Event> eventList;
-    Ui::MainWindow *ui;
-    bool currentToggleNew = false;
+    QVector<Event> eventList; /*!< The list of the events currently established. */
+    Ui::MainWindow *ui; /*!< The "Main Window" object where all of the widgets reside.*/
+    bool currentToggleNew; /*!< Keeps track of whether or not the toggle button on the pageNewTime is currently in the
+                                UTC or the AM/PM format. False represents 24 hour, and true represents 12 hour format. */
 };
 
 #endif // MAINWINDOW_H
